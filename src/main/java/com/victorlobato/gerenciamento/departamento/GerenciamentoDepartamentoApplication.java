c4a512a2a74873e3d0db33de219f4c02bff786bf
Victor Lobato <victorsilva123190@gmail.com>
2022-02-13 09:56:28 -0300

    Adicionando projeto

---
+++ src/main/java/com/victorlobato/gerenciamento/departamento/GerenciamentoDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.victorlobato.gerenciamento.departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDepartamentoApplication.class, args);
+	}
+
+}
