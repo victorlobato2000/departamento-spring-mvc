fa9a9659fed0a50ed38a16e54904a96974188259
Victor Lobato <victorsilva123190@gmail.com>
2022-02-18 19:13:59 -0300

    Inserindo departamentos

--- src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/DepartamentoController.java
+++ src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/DepartamentoController.java
@@ -1,20 +1,33 @@
 package com.victorlobato.gerenciamento.departamento.web.controller;
 
+import com.victorlobato.gerenciamento.departamento.domain.Departamento;
+import com.victorlobato.gerenciamento.departamento.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar(){
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
