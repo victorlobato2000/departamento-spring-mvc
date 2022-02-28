8ce08344c5409557a65b9b1561f4cbf43f58c088
Victor Lobato <victorsilva123190@gmail.com>
2022-02-28 13:03:59 -0300

    Editando departamentos

--- src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/DepartamentoController.java
+++ src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/DepartamentoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,6 +31,18 @@ public class DepartamentoController {
     @PostMapping("/salvar")
     public String salvar(Departamento departamento){
         service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        service.editar(departamento);
         return "redirect:/departamentos/cadastrar";
     }
 }
