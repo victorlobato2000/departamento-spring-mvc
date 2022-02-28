b9c053889877666abb6ebcd5cf680bb7ce90f7b4
Victor Lobato <victorsilva123190@gmail.com>
2022-02-28 15:29:33 -0300

    Editando cargos

--- src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/CargoController.java
+++ src/main/java/com/victorlobato/gerenciamento/departamento/web/controller/CargoController.java
@@ -8,10 +8,7 @@ import com.victorlobato.gerenciamento.departamento.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -41,6 +38,18 @@ public class CargoController {
     public String salvar(Cargo cargo, RedirectAttributes attr){
         cargoService.salvar(cargo);
         attr.addFlashAttribute("success","Cargo inserido com sucesso.");
+        return "redirect:/cargos/cadastrar";
+    }
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("cargo", cargoService.buscarPorId(id));
+        return "/cargo/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Cargo cargo, RedirectAttributes attr) {
+        cargoService.editar(cargo);
+        attr.addFlashAttribute("success","Cargo editado com sucesso.");
         return "redirect:/cargos/cadastrar";
     }
 
